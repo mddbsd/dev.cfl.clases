96a9f1d17791cfe165f491b68633db50c394748b
Maxi <mdd.bsd@gmail.com>
2025-11-03 10:22:50 -0300

    Instalado e implementado react-router con 2 rutas. El componente de objetos no me gusto, lo voy a cambiar todo

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,31 @@
 //Imports Dependencias
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-//Import Componentes
-import Inicio from './componentes/inicio'
-import Header from './componentes/header'
-import Pie from './componentes/pie'
-//Imports CSS
-import './css/general.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router';;
 
+//Import Componentes
+import Inicio from './componentes/inicio';
+import Header from './componentes/header';
+import Pie from './componentes/pie';
+//Imports CSS
+import './css/general.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Inicio
+  },
+  {
+    path: "/formulario",
+    element: <h1>Futura pagina de formulario</h1>  
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <Inicio />
+    <RouterProvider router={router} />
     <Pie />
   </StrictMode>,
 )
